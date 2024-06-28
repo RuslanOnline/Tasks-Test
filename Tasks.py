f836a21b11cb7a603b0f0b535be90498b5a5bdd2
RuslanOnline <scaineton@gmail.com>
2024-06-28 18:04:15 +0300

    Add files via upload

---
+++ Tasks.py
@@ -0,0 +1,12 @@
+         #Task 1
+#"1st program".
+print (9**0.5*5) # 15
+         #Task 2
+#"2nd program".
+print(9.99>9.98)and(1000!=1000.1) #True
+         #Task 3
+#"3rd program".
+print(1234//10%100+5678//10%100) # 23+67=90
+        #Task 4
+#"4th program".
+print (int (13.42)==42.13*100%100) # True
